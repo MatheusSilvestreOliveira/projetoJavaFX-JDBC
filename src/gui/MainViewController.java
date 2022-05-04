31bdf44ce3b6553c1c1421fb5ade20a4bec7f014
Matheus Silvestre <mathsylvester13@gmail.com>
2022-05-04 10:44:52 -0300

    About view

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,13 +1,22 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
-public class MainViewController implements Initializable{
+public class MainViewController implements Initializable {
 
 	@FXML
 	private MenuItem menuItemSeller;
@@ -15,25 +24,45 @@ public class MainViewController implements Initializable{
 	private MenuItem menuItemDeprtament;
 	@FXML
 	private MenuItem menuItemAbout;
-	
+
 	@FXML
 	public void onMenuItemSellerAction() {
 		System.out.println("onMenuItemSellerAction");
 	}
-	
+
 	@FXML
 	public void onMenuItemDepartmentAction() {
 		System.out.println("onMenuItemDepartmentAction");
 	}
-	
+
 	@FXML
 	public void onMenuItemAboutAction() {
-		System.out.println("onMenuItemAboutAction");
+		loadView("/gui/About.fxml");
 	}
-	
+
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
+	}
 
+	private synchronized void loadView(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			//Recupera a cena principal
+			Scene mainScene = Main.getMainScene();
+			//Recupera a VBox da cena principal para alterar a visualização
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			//Recupera a barra de menu pois ela será preservada
+			Node mainMenu = mainVBox.getChildren().get(0);
+			
+			mainVBox.getChildren().clear(); //Limpa todo o conteudo da cena
+			mainVBox.getChildren().add(mainMenu); //Adiciona o menu preservado
+			mainVBox.getChildren().addAll(newVBox.getChildren()); //Adiciona o conteudo novo a cena
+			
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
