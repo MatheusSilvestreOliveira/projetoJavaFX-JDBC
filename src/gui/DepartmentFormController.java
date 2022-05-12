569ab44932d0efb068210a18ac5228856717fc83
Matheus Silvestre <mathsylvester13@gmail.com>
2022-05-12 14:11:09 -0300

    Observer design para atualizar tabela

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
@@ -19,6 +22,12 @@ import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable{
 
+	private Department entity;
+	
+	private DepartmentService service;
+	
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();	
+	
 	@FXML
 	private TextField txtId;
 	@FXML
@@ -30,16 +39,16 @@ public class DepartmentFormController implements Initializable{
 	@FXML
 	private Button btCancel;
 	
-	private Department entity;
-	
-	private DepartmentService service;
-	
 	public void setDepartment(Department entity) {
 		this.entity = entity;
 	}
 	
 	public void setDepartmentService(DepartmentService service) {
 		this.service = service;
+	}
+	
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
 	}
 	
 	@FXML
@@ -53,12 +62,19 @@ public class DepartmentFormController implements Initializable{
 		try {
 			entity = getFormData();
 			service.saveOrUpdate(entity);
+			notifyDataChangeListeners();
 			Utils.currentStage(event).close();
 		} catch (DbException e) {
 			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
 		}
 	}
 	
+	private void notifyDataChangeListeners() {
+		for (DataChangeListener listeners: dataChangeListeners) {
+			listeners.onDataChanged();
+		}		
+	}
+
 	private Department getFormData() {
 		Department obj = new Department();
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
